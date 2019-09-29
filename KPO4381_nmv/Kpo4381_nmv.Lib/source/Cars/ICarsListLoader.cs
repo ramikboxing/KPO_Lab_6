53bf4cf25639a6f3c8e3cefc266809ed39aabb44
ramikboxing <45014154+ramikboxing@users.noreply.github.com>
2019-09-29 22:06:44 +0300

    Add files via upload

---
+++ KPO4381_nmv/Kpo4381_nmv.Lib/source/Cars/ICarsListLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Kpo4381_nmv.Lib
+{
+    public interface ICarsListLoader
+    {
+        List<Car> carsList { get; }
+        LoadStatus status { get; }
+
+        void Execute();
+        void SetOfDeligate(OnLoadFileDelegate onAfterRowConvert);
+    }
+    /*public enum LoadStatus
+    {
+        None =-1,
+        ReadingData =0,
+        Success=1,
+        ReadingError=2
+    }*/
+}
