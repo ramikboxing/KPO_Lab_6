d6c8556521cca8649f3c5a8982cfc8f8d9324175
ramikboxing <45014154+ramikboxing@users.noreply.github.com>
2019-09-29 22:10:13 +0300

    Add files via upload

---
+++ KPO4381_nmv/KPO4381_nmv/Kpo4381_nmv.Lib/source/Log/LogUtility.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Kpo4381_nmv.Utility
+{
+    public static class LogUtility
+    {
+        public static void ErrorLog(string message)
+        {
+            string path = @"C:\VS\ErrorLog.txt"; // Расположение файла с сохр-ми ошибками
+            File.AppendAllText(path, DateTime.Now + "-" + message + "\r\n");
+        }
+        public static void ErrorLog(Exception ex)
+        {
+            string path = @"C:\VS\ErrorLog1.txt";
+            File.AppendAllText(path, DateTime.Now + "-" + ex.Message + "\r\n");
+        }
+    }
+}
---
+++ KPO4381_nmv/UnitTestProject/UnitTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Kpo4381_nmv.Lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+//namespace UnitTestProject
+namespace Kpo4381_nmv
+{
+    [TestClass]
+    public class UnitTest
+    {
+        private CarNewLoad_lab4 newLoader;
+             
+        public UnitTest()
+        {
+            newLoader = new CarNewLoad_lab4("Lab4Cars.txt");
+        }
+        //Test 1
+        /*Проверка имени файла для загрузки*/
+        [TestMethod]
+        public void TestFileName()
+        {
+            var target = newLoader.getFileName;
+            Assert.AreEqual("Lab4Cars.txt", target);
+        }
+        //Тест 2
+        /*Проверка марки первой ммашины в получаемом списке*/
+        [TestMethod]
+        public void TestExecuteMethodCarModel()
+        {
+            newLoader.Execute();
+            var targetCarModel = newLoader.carsList[0].Mark;
+            Console.WriteLine("TestTest"+ newLoader.carsList[0].Mark);
+            Assert.AreEqual("Mersedes", targetCarModel);
+        }
+        //Тест3
+        /*Проверка на возврат не путого списка*/
+        [TestMethod]
+        public void CheckListNotNull_Class()
+        {
+            LoadCarListTest list = new LoadCarListTest();
+            list.Execute(); //Вывзвать метот создающий и заполняющий список
+            Assert.IsNotNull(list.carsList); //Если срисок есть то ОК
+        }
+        //Тест 4
+        /*Проверка пустой список из класса или нет*/
+        [TestMethod]
+        public void ChekListNotNull_File()
+        {
+            ICarFactory loader = new CarTestFactory(); // обращение к объектам через интер-с
+            ICarsListLoader Loader = loader.CreateCarListLoader();
+            Loader.Execute();// Заполнение списка
+            Assert.IsNotNull(Loader.carsList);// Проверка списка на пустату          
+        }
+        //Тест 5
+        /**/
+        [TestMethod]
+        public void ChekListWithStatusSuccess_TestFileLoad()
+        {   
+            ICarFactory loader = new CarSplitFileFactory(); // обращение к объекту через интерфейс
+            ICarsListLoader Loader = loader.CreateCarListLoader();
+            /*Список удачно читается когда в классе CarSplitFileFactory явно передано имя файла
+            для чтения, при передачи имени файла через AppGlobalSettin возникает исключения
+            "Файл не найден" т.к. имя файла прочитываеться из App.config в переменную _dataFileName*/
+            Loader.Execute();
+            /*Если файл найден стуст Succes иначе ReadingError*/
+            if (Loader.status == LoadStatus.Succes)
+            {
+                Assert.IsTrue(true);
+            }
+            else
+            {
+                Assert.IsTrue(false);
+            }
+        }
+    }
+}
+
